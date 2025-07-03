02ec4858b71ef43c33c519fc5b26b073ec57bc74
Endimionx <endimion7@gmail.com>
2025-07-04 01:53:29 +0700

    Update ai_model.py pisahkan train

--- ai_model.py
+++ ai_model.py
@@ -1,14 +1,26 @@
+# ai_model.py
+
 import numpy as np
+import streamlit as st
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.utils import to_categorical
 
-def top5_lstm(df):
+@st.cache_resource
+def train_lstm_model(X, y):
+    model = Sequential()
+    model.add(LSTM(128, return_sequences=False, input_shape=(10, 4)))
+    model.add(Dense(40, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy')
+    model.fit(X, y, epochs=50, batch_size=16, verbose=0, validation_split=0.1)
+    return model
+
+def prepare_lstm_data(df):
     data = df['angka'].dropna().apply(lambda x: [int(d) for d in f"{int(x):04d}"]).tolist()
     data = np.array(data)
 
     if len(data) < 11:
-        return None
+        return None, None, None
 
     X, y = [], []
     for i in range(len(data) - 10):
@@ -16,21 +28,19 @@ def top5_lstm(df):
         y.append(data[i+10])
     X, y = np.array(X), np.array(y)
 
-    if len(X) == 0 or len(y) == 0:
-        return None
-
     y_encoded = np.array([to_categorical(d, num_classes=10) for d in y])
     y_encoded = y_encoded.reshape(-1, 40)
 
-    model = Sequential()
-    model.add(LSTM(128, return_sequences=False, input_shape=(10, 4)))
-    model.add(Dense(40, activation='softmax'))
-    model.compile(optimizer='adam', loss='categorical_crossentropy')
-
-    val_split = 0.1 if len(X) >= 10 else 0
-    model.fit(X, y_encoded, epochs=50, batch_size=16, verbose=0, validation_split=val_split)
-
     input_seq = np.array(data[-10:]).reshape(1, 10, 4)
+
+    return X, y_encoded, input_seq
+
+def top5_lstm(df):
+    X, y, input_seq = prepare_lstm_data(df)
+    if X is None or y is None or input_seq is None:
+        return [[0], [0], [0], [0]]
+
+    model = train_lstm_model(X, y)
     pred = model.predict(input_seq, verbose=0)[0].reshape(4, 10)
 
     top5 = []
