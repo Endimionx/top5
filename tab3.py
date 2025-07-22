cd6de596c88fa8193c8c465935ed25b6b62f9bee
Endimionx <endimion7@gmail.com>
2025-07-22 18:24:40 +0700

    Update tab3.py

--- tab3.py
+++ tab3.py
@@ -110,15 +110,15 @@ def tab3(df):
                 st.session_state.tab3_ensemble[label] = final_ens_conf
 
                 # === Fix for ensemble_probabilistic ===
-                all_lstm_top6 = []
-                catboost_accs = []
-                for ws, (_, probs) in lstm_dict.items():
+                # Benar
+                for ws in lstm_dict:
+                    top6, probs = lstm_dict[ws]
                     if len(probs) > 0:
                         all_lstm_top6.append(probs)
                         acc_row = result_df[result_df["WS"] == ws]
                         if not acc_row.empty:
                             catboost_accs.append(acc_row["Accuracy Mean"].values[0])
-
+                            
                 if all_lstm_top6 and catboost_accs:
                     final_ens_prob = ensemble_probabilistic(all_lstm_top6, catboost_accs)
                     st.session_state.tab3_ensemble_prob[label] = final_ens_prob
