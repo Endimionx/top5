6460d7f1450e8048e5bec20c1238aee83ec706a8
Endimionx <endimion7@gmail.com>
2025-07-24 05:29:17 +0700

    Update tab3.py

--- tab3.py
+++ tab3.py
@@ -84,7 +84,7 @@ def dynamic_alpha(acc_conf, acc_prob):
         return 0.5
     return acc_conf / (acc_conf + acc_prob)
 
-def log_prediction(label, conf, prob, hybrid, alpha, stacked=None, final=none, lokasi=None):
+def log_prediction(label, conf, prob, hybrid, alpha, stacked=None, final=None, lokasi=None):
     log_path = "log_tab3.txt"
     with open(log_path, "a") as f:
         if lokasi:
